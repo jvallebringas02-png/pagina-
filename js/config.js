a66cc78154a65603f05cd21580132f58ff53e044
jvallebringas02-png <jvallebringas02@gmail.com>
2026-08-28 18:31:04 -0500

    Update GROQ_API_KEY in config.js

--- js/config.js
+++ js/config.js
@@ -9,7 +9,7 @@ export const CONFIG = {
     SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtxYXprcmF4bHFuY2Zjd2Jic3BzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE4MzA0NDEsImV4cCI6MjA5NzQwNjQ0MX0.C4cdFMTPNAjjBahz5xBl9s1OpnqyvQVrUmzVYCreiLoi",
   
   // Groq API (IA)
-    GROQ_API_KEY: 'gsk_IG41lb4B4QoFsJDik0tZWGdyb3F5FREU3wIVe10lG7KAJEb8vs3',
+    GROQ_API_KEY: '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',
     EDGE_FUNCTION_CHAT: 'chat-ia',
     
   // Categorías disponibles
